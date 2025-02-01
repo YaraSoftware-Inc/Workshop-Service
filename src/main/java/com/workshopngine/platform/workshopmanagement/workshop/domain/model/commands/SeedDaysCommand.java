f670244fedb3006ceb67f04cb2e706537da80f9f
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-01-31 23:16:25 -0500

    refactor: remove unnecessary entity Day

--- src/main/java/com/workshopngine/platform/workshopmanagement/workshop/domain/model/commands/SeedDaysCommand.java
+++
@@ -1,3 +0,0 @@
-package com.workshopngine.platform.workshopmanagement.workshop.domain.model.commands;
-
-public record SeedDaysCommand() {}
--- src/main/java/com/workshopngine/platform/workshopmanagement/workshop/domain/services/DayCommandService.java
+++
@@ -1,7 +0,0 @@
-package com.workshopngine.platform.workshopmanagement.workshop.domain.services;
-
-import com.workshopngine.platform.workshopmanagement.workshop.domain.model.commands.SeedDaysCommand;
-
-public interface DayCommandService {
-    void handle(SeedDaysCommand command);
-}
