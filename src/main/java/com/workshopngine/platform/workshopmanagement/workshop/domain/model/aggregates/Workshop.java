c911ff3ee58fc5ba3fdaa32aff4b274fdb0ae2d7
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-01-31 20:31:01 -0500

    feat: add Worshop aggregate

---
+++ src/main/java/com/workshopngine/platform/workshopmanagement/workshop/domain/model/aggregates/Workshop.java
@@ -0,0 +1,53 @@
+package com.workshopngine.platform.workshopmanagement.workshop.domain.model.aggregates;
+
+import com.workshopngine.platform.workshopmanagement.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import com.workshopngine.platform.workshopmanagement.workshop.domain.model.commands.CreateWorkshopCommand;
+import com.workshopngine.platform.workshopmanagement.workshop.domain.model.commands.UpdateWorkshopCommand;
+import com.workshopngine.platform.workshopmanagement.workshop.domain.model.valueobjects.*;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Workshop extends AuditableAbstractAggregateRoot<Workshop> {
+    @Embedded
+    private WorkshopInfo information;
+
+    @Embedded
+    private Location location;
+
+    @Embedded
+    private WorkingSchedule schedule;
+
+    @Embedded
+    private Capacity capacity;
+
+    @Embedded
+    private OwnerId ownerId;
+
+    private WorkshopStatus status;
+
+    public Workshop() {
+        super();
+        this.status = WorkshopStatus.REGISTERED;
+    }
+
+    public Workshop(CreateWorkshopCommand command){
+        this();
+        this.information = command.information();
+        this.location = command.location();
+        this.schedule = command.schedule();
+        this.capacity = command.capacity();
+        this.ownerId = command.ownerId();
+    }
+
+    public void update(UpdateWorkshopCommand command) {
+        this.information = command.information();
+        this.location = command.location();
+        this.schedule = command.schedule();
+        this.capacity = command.capacity();
+    }
+}
