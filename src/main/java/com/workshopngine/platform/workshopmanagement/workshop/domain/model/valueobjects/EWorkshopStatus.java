669e4e907a2e2737e88fcafcde361e5d4ae6039a
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-01 00:00:41 -0500

    feat: add method to verify and convert string to enum

--- src/main/java/com/workshopngine/platform/workshopmanagement/workshop/domain/model/valueobjects/EWorkshopStatus.java
+++ src/main/java/com/workshopngine/platform/workshopmanagement/workshop/domain/model/valueobjects/EWorkshopStatus.java
@@ -4,5 +4,14 @@ public enum EWorkshopStatus {
     CLOSED,
     OPEN,
     FULL_CAPACITY,
-    OUT_OF_SERVICE
+    OUT_OF_SERVICE;
+
+    public static EWorkshopStatus fromString(String status) {
+        for (EWorkshopStatus s : EWorkshopStatus.values()) {
+            if (s.name().equalsIgnoreCase(status)) {
+                return s;
+            }
+        }
+        throw new IllegalArgumentException("Invalid status: " + status);
+    }
 }
