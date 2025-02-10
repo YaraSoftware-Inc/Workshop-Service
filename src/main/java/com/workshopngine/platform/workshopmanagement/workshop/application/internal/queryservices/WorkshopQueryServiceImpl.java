bee10dbe158fedda827bac460e60b444bec66956
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-02-09 19:15:53 -0500

    feat: add query method tp verify availabilty in WorkshopQueryServiceImpl

--- src/main/java/com/workshopngine/platform/workshopmanagement/workshop/application/internal/queryservices/WorkshopQueryServiceImpl.java
+++ src/main/java/com/workshopngine/platform/workshopmanagement/workshop/application/internal/queryservices/WorkshopQueryServiceImpl.java
@@ -5,13 +5,13 @@ import com.workshopngine.platform.workshopmanagement.workshop.domain.model.entit
 import com.workshopngine.platform.workshopmanagement.workshop.domain.model.queries.GetAllWorkingDaysByWorkshopIdQuery;
 import com.workshopngine.platform.workshopmanagement.workshop.domain.model.queries.GetWorkshopByIdQuery;
 import com.workshopngine.platform.workshopmanagement.workshop.domain.model.queries.GetWorkshopByOwnerIdQuery;
+import com.workshopngine.platform.workshopmanagement.workshop.domain.model.queries.IsAvailableWorkshopByIdAndRequestedTimeQuery;
 import com.workshopngine.platform.workshopmanagement.workshop.domain.model.valueobjects.OwnerId;
 import com.workshopngine.platform.workshopmanagement.workshop.domain.services.WorkshopQueryService;
 import com.workshopngine.platform.workshopmanagement.workshop.infrastructure.persistence.jpa.repositories.WorkshopRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -38,5 +38,12 @@ public class WorkshopQueryServiceImpl implements WorkshopQueryService {
         var workshop = workshopRepository.findById(query.workshopId());
         if (workshop.isEmpty()) throw new IllegalArgumentException("Workshop with ID %s not found".formatted(query.workshopId()));
         return workshop.get().getWorkingSchedule().getWorkingDays();
+    }
+
+    @Override
+    public Boolean handle(IsAvailableWorkshopByIdAndRequestedTimeQuery query) {
+        var workshop = workshopRepository.findById(query.workshopId());
+        if (workshop.isEmpty()) return false;
+        return workshop.get().getWorkingSchedule().isAvailableAtRequestedTime(query.requestedTime());
     }
 }
